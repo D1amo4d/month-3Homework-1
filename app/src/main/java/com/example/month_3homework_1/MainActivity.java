c6e56acc14ba1bae7f5312db4ec420aa9be9e493
D1amo4d <147538683+D1amo4d@users.noreply.github.com>
2023-10-12 18:08:34 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/month_3homework_1/MainActivity.java
+++ app/src/main/java/com/example/month_3homework_1/MainActivity.java
@@ -1,53 +1,55 @@
 package com.example.month_3homework_1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textfield.TextInputEditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-//16:15
-    TextInputEditText etGmail;
-    TextInputEditText etTheme;
-    TextInputEditText etSms;
-    MaterialButton btnSend;
-    MaterialButton btnExit;
+
+    private EditText editTextTo;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-        btnClicer();
 
-    }
-    private void initView() {
-        etGmail=findViewById(R.id.text_gmail);
-        etTheme=findViewById(R.id.text_theme);
-        etSms=findViewById(R.id.text_sms);
-        btnSend=findViewById(R.id.btn_send);
-        btnSend=findViewById(R.id.btn_exit);
-    }
-    private void btnClicer() {
-        String gmail etGmail.getText().toString();
-        String theme etTheme.getText().toString();
-        String sms etSms.getText().toString();
+        editTextTo = findViewById(R.id.text_gmail);
+        editTextSubject = findViewById(R.id.text_theme);
+        editTextMessage = findViewById(R.id.text_sms);
 
-        btnSend.setOnClickListener(view -> {
-            Intent intent=new Intent(Intent.ACTION_GET_CONTENT  );
-            intent.putExtra(Intent.EXTRA_EMAIL,new String[]{String.valueOf(gmail)});
-            intent.putExtra(Intent.EXTRA_SUBJECT, (CharSequence) theme);
-            intent.putExtra(Intent.EXTRA_TEXT, (CharSequence) sms);
-            intent.setType("message/rfc822");
-            startActivity(Intent.createChooser(intent,"WYBERITE SOBSENIE"));
+        Button buttonSend = findViewById(R.id.btn_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String to = editTextTo.getText().toString();
+                String subject = editTextSubject.getText().toString();
+                String message = editTextMessage.getText().toString();
+
+                sendEmail(to, subject, message);
+            }
         });
+    }
 
-        btnExit.setOnClickListener(view -> {
-            finish();
-        });
+    private void sendEmail(String to, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        try {
+            startActivity(Intent.createChooser(intent, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Обработка случая, когда нет приложения для отправки электронной почты
+        }
     }
 }
+
